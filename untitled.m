2b1d37bf1a6ff251a6be4aa16cf34299b8decc47
festepaq <festepaq@unal.edu.co>
2021-11-14 13:01:02 -0500

    Final del punto 5

--- untitled.m
+++ untitled.m
@@ -17,3 +17,62 @@ figure()
 Robot_ABB.plot([0,0,0,0,0,0],'workspace',[-0.5 0.5 -0.5 0.5 -0.1 1],'scale',0.5,'noa', 'view',[140 20])
 
 %%
+
+clear all; close all;
+L1=0.265; L2=0.444; L3=0.110; L4=0.470; L5=0.080; L6=0.037;
+mdhparams = [0   0   	L1   	0;
+            0	-pi/2   0       0;
+            L2	0   	0   	0;
+            L3  -pi/2	L4   	0;
+            0   pi/2	0   	0;
+            L5  -pi/2   L6      0];
+robot = rigidBodyTree;
+
+body1 = rigidBody('body1');
+jnt1 = rigidBodyJoint('jnt1','revolute');
+
+setFixedTransform(jnt1,mdhparams(1,:),'mdh');
+body1.Joint = jnt1;
+
+addBody(robot,body1,'base')
+
+body2 = rigidBody('body2');
+jnt2 = rigidBodyJoint('jnt2','revolute');
+body3 = rigidBody('body3');
+jnt3 = rigidBodyJoint('jnt3','revolute');
+body4 = rigidBody('body4');
+jnt4 = rigidBodyJoint('jnt4','revolute');
+body5 = rigidBody('body5');
+jnt5 = rigidBodyJoint('jnt5','revolute');
+body6 = rigidBody('body6');
+jnt6 = rigidBodyJoint('jnt6','revolute');
+
+
+setFixedTransform(jnt2,mdhparams(2,:),'mdh');
+setFixedTransform(jnt3,mdhparams(3,:),'mdh');
+setFixedTransform(jnt4,mdhparams(4,:),'mdh');
+setFixedTransform(jnt5,mdhparams(5,:),'mdh');
+setFixedTransform(jnt6,mdhparams(6,:),'mdh');
+
+
+
+body2.Joint = jnt2;
+body3.Joint = jnt3;
+body4.Joint = jnt4;
+body5.Joint = jnt5;
+body6.Joint = jnt6;
+
+
+addBody(robot,body2,'body1')
+addBody(robot,body3,'body2')
+addBody(robot,body4,'body3')
+addBody(robot,body5,'body4')
+addBody(robot,body6,'body5')
+
+
+
+showdetails(robot)
+config=homeConfiguration(robot);
+config(2).JointPosition = -pi/2;
+show(robot,config);
+axis([-1,1,-1,1,-1,1])
