7199ff5b2d8ea6f0d6a07b6d8311ae02537cb7c2
williamdres23 <87773934+williamdres23@users.noreply.github.com>
2021-11-14 20:54:31 -0500

    Se agregó el eje X para herramienta para la configuración de q5

--- App_ABB_CRB15000.m
+++ App_ABB_CRB15000.m
@@ -479,6 +479,12 @@ classdef App_ABB_CRB15000 < matlab.apps.AppBase
            pos_tool_z = [pos_z(7) tool(3)];
            plot3(app.grafica,pos_tool_x,pos_tool_y,pos_tool_z,'LineWidth',2,'Color','r');
 
+           tool = MTH_BC*[0 0 0.2 1]';
+           pos_tool_x = [pos_x(7) tool(1)];
+           pos_tool_y = [pos_y(7) tool(2)];
+           pos_tool_z = [pos_z(7) tool(3)];
+           plot3(app.grafica,pos_tool_x,pos_tool_y,pos_tool_z,'LineWidth',2,'Color','c');
+
            view(app.grafica,140,30);
            axis(app.grafica,[-1 1 -1 1 -0.67 1.3]);
            hold(app.grafica,'off');
