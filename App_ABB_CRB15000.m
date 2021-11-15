7a737c3083e25efc8eb4f8ee9e4f39d001e0fcbe
williamdres23 <87773934+williamdres23@users.noreply.github.com>
2021-11-14 20:41:05 -0500

    Se agregaron los ejes en la herramienta para q3

--- App_ABB_CRB15000.m
+++ App_ABB_CRB15000.m
@@ -160,9 +160,6 @@ classdef App_ABB_CRB15000 < matlab.apps.AppBase
            end
 
            plot3(app.grafica,pos_x,pos_y,pos_z,'LineWidth',5,'Color',[.6 0 0]);
-           view(app.grafica,140,30);
-           axis(app.grafica,[-1 1 -1 1 -0.67 1.3]);
-           hold(app.grafica,'off');
 
            tool = MTH_BC*[0.15 0 0 1]';
            pos_tool_x = [pos_x(7) tool(1)];
@@ -175,6 +172,10 @@ classdef App_ABB_CRB15000 < matlab.apps.AppBase
            pos_tool_y = [pos_y(7) tool(2)];
            pos_tool_z = [pos_z(7) tool(3)];
            plot3(app.grafica,pos_tool_x,pos_tool_y,pos_tool_z,'LineWidth',2,'Color','c');
+
+           view(app.grafica,140,30);
+           axis(app.grafica,[-1 1 -1 1 -0.67 1.3]);
+           hold(app.grafica,'off');
 
            app.info_q1.Value = round(q_1*180/pi,2);
            app.info_q2.Value = round(q_2*180/pi,2);
