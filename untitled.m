bb8186ca1af4bb1aeb75f64545304c50e1564771
festepaq <festepaq@unal.edu.co>
2021-11-14 21:11:57 -0500

    Ultima

--- untitled.m
+++ untitled.m
@@ -10,10 +10,11 @@ L(3) = Link('revolute','alpha', 0,    'a', L2,   'd',0,   'offset', 0,   'modifi
 L(4) = Link('revolute','alpha', -pi/2,    'a', L3,   'd',L4,   'offset', 0,   'modified', 'qlim',[-pi pi]);
 L(5) = Link('revolute','alpha', pi/2,    'a', 0,   'd',0,   'offset', 0,   'modified', 'qlim',[-pi pi]);
 L(6) = Link('revolute','alpha', -pi/2,    'a', L5,   'd',L6,   'offset', 0,   'modified', 'qlim',[-pi pi]);
-%plot_options = {'workspace',[-2 2 -2 2 -2 2],'scale',0.05, 'view',[140 20]};
+
 Robot_ABB = SerialLink(L,'name','ABB CRB 15000');
 %figure()
 Robot_ABB.plot([0,0,0,0,0,0],'workspace',[-0.5 0.5 -0.5 0.5 -0.1 1],'scale',0.5,'noa', 'view',[140 20])
+plot_options = {'workspace',[-2 2 -2 2 -8 2],'scale',0.05, 'view',[140 20]};
 Robot_ABB.teach([0 0 0 0 0 0]);
 
 %%
